337173b68a10b148c2929a91f24c4048607f1b19
test person <test@test.net>
2023-08-22 23:26:25 +0530

    Set Bearer token as global variable in Postman

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,8 +2,15 @@
 const Job = require('../models/Job')
 const {StatusCodes} = require('http-status-codes')
 const { BadRequestError,NotFoundError } = require('../errors')
+
+
 const getAllJobs = async (req, res) => {
-    res.send('Get all jobs')
+
+    const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({
+        jobs,
+        count:jobs.length
+    })
 }
 const getJob = async (req, res) => {
     res.send('Get  job')
