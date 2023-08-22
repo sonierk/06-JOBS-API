068f95ac989ae1088a2213e3e46ad0f7bcc14373
test person <test@test.net>
2023-08-22 20:43:02 +0530

    Implement Register function, jwt token generationn

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,6 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
 const register = async (req, res) => {
     // const { name, email, password } = req.body
     // const salt = await bcrypt.genSalt(10)
@@ -13,7 +12,8 @@ const register = async (req, res) => {
     //     throw new BadRequestError('Please provide name, email and password')
     // }
     const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json({user})
+    const token = user.createJWT() 
+    res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
 }
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -30,4 +31,8 @@ UserSchema.pre('save', async function(next){
     next()
 })
 
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({userid:this._id, name:this.name},process.env.JWT_SECRET,{expiresIn: process.env.JWT_LIFETIME})
+}
+ 
 module.exports = mongoose.model('User',UserSchema)
